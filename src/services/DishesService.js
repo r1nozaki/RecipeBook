0cdabd666f1dd7a03212073f7d204e9c31847d87
r1nozaki <r1nozaki@gmail.com>
2025-08-13 19:49:43 +0300

    Update DishesService

--- src/services/DishesService.js
+++ src/services/DishesService.js
@@ -3,5 +3,26 @@ import { useHttp } from '../hooks/http.hook';
 const DishesService = () => {
   const { request, clearError, process, setProcess } = useHttp();
 
-  const _apiBase = '';
+  const _apiBase = 'https://api.spoonacular.com/recipes/complexSearch';
+  const _apiKey = 'de21ebb523954e5dbdf00001b6429765';
+
+  const getPopularRecipes = async () => {
+    const res = await request(
+      `${_apiBase}?query=waffles,pancakes,hot%20chocolate&number=9&addRecipeInformation=true&apiKey=${_apiKey}`
+    );
+    return res.results.map(_transformDishCard);
+  };
+
+  const _transformDishCard = dish => {
+    return {
+      id: dish.id,
+      image: dish.image,
+      title: dish.title,
+      content: dish.summary.replace(/<[^>]+>/g, ''),
+    };
+  };
+
+  return { clearError, process, setProcess, getPopularRecipes };
 };
+
+export default DishesService;
